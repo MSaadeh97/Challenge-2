4a3eb945f0386d83047de3d5c65732e3a914fc7d
MSaadeh97 <59637290+MSaadeh97@users.noreply.github.com>
2020-03-04 16:39:10 -0500

    moe

---
+++ Challenge 2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector2 a, b;
+    [Range(0, 1)]
+    public float speed = 1.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(a, b, Mathf.PingPong(Time.time * speed, 1));
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+    }
+}
